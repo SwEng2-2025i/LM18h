f05cef35bad8deeae080faf7a1cdff7f00ad4a6c
daniel garcia <dany2017305@gmail.com>
2025-05-30 19:05:38 -0500

    laboratorio Carlos Daniel García Chaparro

--- Laboratory1/1000506977/app/models/user.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-class User:
-    def __init__(self, user_id, name, preferred_channel, channels):
-        self.id = user_id or str(uuid.uuid4())
-        self.name = name
-        self.preferred_channel = preferred_channel
-        self.channels = channels or []
